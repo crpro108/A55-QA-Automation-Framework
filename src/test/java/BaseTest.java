bd33b54d2f699939df50d18e96444f2812a61ee1
crpro108 <157779494+crpro108@users.noreply.github.com>
2024-03-03 15:24:22 -0600

    HW17 test adjusted w/ suggestions. Thank you!

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -10,14 +10,15 @@ import java.time.Duration;
 
 public class BaseTest {
     public WebDriver driver;
-    public String url = "https://qa.koel.app/";
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeMethod
-    public void launchBrowser(){
+    @Parameters("baseUrl")
+    public void launchBrowser(String baseUrl){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         //System.setProperty("webdriver.chrome.driver", "chromedriver-mac-arm64/chromedriver");
@@ -25,6 +26,7 @@ public class BaseTest {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        driver.get(baseUrl);
     }
 
     //Helper Methods
@@ -46,9 +48,9 @@ public class BaseTest {
         emailField.sendKeys(email);
     }
 
-    public void navigateToPage() {
-        driver.get(url);
-    }
+//    public void navigateToPage() {
+//        driver.get(url);
+//    }
 
     @AfterMethod
     public void closeBrowser(){
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -5,15 +5,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-
-public class LoginTests extends BaseTest {
-    public String url = "https://qa.koel.app/";
-    @Test
-    public void loginEmptyEmailPassword() {
-        // test for forking
-//      Added ChromeOptions argument below to fix websocket error
-        navigateToPage();
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
-    }
-}
+//
+//public class LoginTests extends BaseTest {
+//    public String url = "https://qa.koel.app/";
+//    @Test
+//    public void loginEmptyEmailPassword() {
+//        // test for forking
+////      Added ChromeOptions argument below to fix websocket error
+//        navigateToPage();
+//        Assert.assertEquals(driver.getCurrentUrl(), url);
+//        driver.quit();
+//    }
+//}
