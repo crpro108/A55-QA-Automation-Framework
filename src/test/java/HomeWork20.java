70880d345eb11ed1f99a5b6658fad8a2b1fe5c5b
crpro108 <157779494+crpro108@users.noreply.github.com>
2024-03-09 09:59:19 -0600

    HW20 for Test1 as playlist and explicit waiting w/ wait.until v3

--- src/test/java/HomeWork20.java
+++ src/test/java/HomeWork20.java
@@ -14,7 +14,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 public class HomeWork20 extends BaseTest{
     @Test
-    public void deletePlayList() throws InterruptedException {
+    public void deletePlayList() {
         String expectedPlayListMsg = "Deleted playlist \"Test1.\"";
 
         provideEmail("charita.ramnathsing@testpro.io");
