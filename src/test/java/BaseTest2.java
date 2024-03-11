de1bd4594af1e415fe3b95ea41637ad577d064a5
crpro108 <157779494+crpro108@users.noreply.github.com>
2024-03-10 19:34:09 -0500

    HW22 adjusted testcase w/ Pages

---
+++ src/test/java/BaseTest2.java
@@ -0,0 +1,58 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class BaseTest2 {
+    public WebDriver driver;
+
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    @Parameters("baseUrl")
+    public void launchBrowser(String baseUrl){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        // version fix
+        System.setProperty("webdriver.chrome.driver", "chromedriver-mac-arm64/chromedriver");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+    }
+
+    //Helper Methods
+//    public void loginToKoel() {
+//        WebElement loginBtn =  driver.findElement(By.cssSelector("button[type='submit']"));
+//        loginBtn.click();
+//        //Thread.sleep(5000);
+//    }
+//
+//    public void providePassword(String password) {
+//        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+//        passwordField.clear();
+//        passwordField.sendKeys(password);
+//    }
+//
+//    public void provideEmail(String email) {
+//        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+//        emailField.clear();
+//        emailField.sendKeys(email);
+//    }
+
+
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+}
